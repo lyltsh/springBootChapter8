e43ec20eb9a2a6460aefc0ca47442016926075e8
leiyl <525082908@qq.com>
2018-08-28 23:36:47 +0800

    chapter8 init

---
+++ src/main/java/com/springboot/javaee/chapter8/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.javaee.chapter8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: leiyulin
+ * @description:
+ * @date:2018/8/28下午11:35
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
